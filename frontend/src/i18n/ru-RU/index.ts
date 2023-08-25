a58589f2885458d67bcd173a9a6a5cd5416f0dd8
Nitamet <13808955+Nitamet@users.noreply.github.com>
2023-08-25 23:55:18 +0300

    feat(i18n): add russian langauge

---
+++ frontend/src/i18n/ru-RU/index.ts
@@ -0,0 +1,18 @@
+export default {
+    gameNotLaunched: 'League of Legends has not been launched.',
+    notSupportedGameMode: 'This game mode is not supported.',
+    notInLobby: 'You are not in lobby.',
+    inGame: 'You are in game.',
+    unknownState: 'Unknown state.',
+    errorOccurred: 'An error occurred. Try to restart the app.',
+    inLobby: 'In Lobby',
+    importSelectedBuild: 'Import Selected Build',
+    runes: 'Runes',
+    items: 'Items',
+    spells: 'Spells',
+    autoUpdateOption: 'Auto update (restart required to take effect)',
+    showNativeTitleBarOption:
+        'Show native title bar (restart required to take effect)',
+    autoImportSelectedBuildOption: 'Auto import selected build',
+    appVersion: 'App Version',
+};
